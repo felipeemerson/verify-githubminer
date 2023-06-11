a7a33aac58743711a589ca40214ff587997467f5
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-06-11 14:22:24 -0300

    init commit

---
+++ src/main/java/com/example/mainservice/controller/MainController.java
@@ -0,0 +1,33 @@
+package com.example.mainservice.controller;
+
+import com.example.mainservice.entity.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    private List<Book> books;
+
+    public MainController() {
+        this.books = List.of(
+                new Book(1L, "Felipe", "Investigating bug reports"),
+                new Book(2L, "Marcio", "Hiring at Dell")
+        );
+    }
+
+    @GetMapping("/")
+    public String getHome() {
+        return "Hello World from main service!";
+    }
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getBooks() {
+        return new ResponseEntity<>(this.books, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/mainservice/entity/Book.java
@@ -0,0 +1,18 @@
+package com.example.mainservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    private Long id;
+    private String author;
+    private String title;
+
+}
