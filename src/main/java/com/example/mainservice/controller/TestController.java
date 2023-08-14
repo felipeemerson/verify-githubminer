5b0ab71f3edc11d0afd9de8035830e732805f37b
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-08-14 09:42:34 -0300

    Commit 1 - Before PR

---
+++ src/main/java/com/example/mainservice/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.mainservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello World - Test Controller";
+    }
+
+}
