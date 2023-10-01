67c55449023589323e077d11d1ae440c0fcfc911
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-10-01 20:35:40 -0300

    commit 2

--- src/main/java/com/example/mainservice/controller/TestController.java
+++ src/main/java/com/example/mainservice/controller/TestController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String getHello() {
-        return "Hello World - Test Controller";
-    }
-
     @PostMapping("/book/{id}")
     public String getBook(@PathVariable(name = "id") String id) {
         return String.format("Book with id %s", id);
+    }
+
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello World - Test Controller";
     }
 
 }
