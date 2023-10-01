f8d0c7de7dfad3c1b5ffeb27f2c1756623145d8e
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2023-10-01 20:25:49 -0300

    add user, usercontroller and personcontroller

---
+++ src/main/java/com/example/mainservice/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.mainservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private String taxId;
+    private Date bithday;
+    private String phoneNumber;
+
+}
